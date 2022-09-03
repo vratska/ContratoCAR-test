e586c7c019df505d3e07d6a02d609ed921451d93
Elias <mr.vratska@gmail.com>
2022-09-03 12:46:02 -0300

    testes...

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ function handleSubmit () {
     const comodanteenderaco = document.getElementById('comodanteenderaco').value;
     const comandatario = document.getElementById('comandatario').value;
     const comandatariocpf = document.getElementById('comandatariocpf').value;
-    const comandatarioenderaco = document.getElementById('comandatarioenderaco').value;
+    const comandatarioendereco = document.getElementById('comandatarioendereco').value;
     
-    localStorage.setItem("COMODANTE", comodante);
-    localStorage.setItem("COMODANTECPF", comodantecpf);
-    localStorage.setItem("COMODANTEENDERECO", comodanteenderaco);
-    localStorage.setItem("COMANDATARIO", comandatario);
-    localStorage.setItem("COMANDATARIOCPF", comandatariocpf);
-    localStorage.setItem("COMANDATARIOENDERECO", comandatarioenderaco);
+    // localStorage.setItem("COMODANTE", comodante);
+    // localStorage.setItem("COMODANTECPF", comodantecpf);
+    // localStorage.setItem("COMODANTEENDERECO", comodanteenderaco);
+    // localStorage.setItem("COMANDATARIO", comandatario);
+    // localStorage.setItem("COMANDATARIOCPF", comandatariocpf);
+    // localStorage.setItem("COMANDATARIOENDERECO", comandatarioendereco);
 
     return;
 }
--- result.js
+++ result.js
@@ -1,15 +1,15 @@
-window.addEventListener('load', () =>{
+window.addEventListener('load', () => {
     const params = (new URL(document.location)).searchParams;
     const comodante = params.get('comodante');
     const comodantecpf = params.get('comodantecpf');
     const comodanteendereco = params.get('comodanteendereco');
     const comandatario = params.get('comandatario');
     const comandatariocpf = params.get('comandatariocpf');
-    const comandatarioenderaco = params.get('comandatarioenderaco');
+    const comandatarioendereco = params.get('comandatarioendereco');
 
     // const comodante = sessionStorage.getItem("COMODANTE");
     // const comodantecpf = sessionStorage.getItem("COMODANTECPF");
-    // const comodanteenderaco = sessionStorage.getItem("COMODANTEENDERECO");
+    // const comodanteendereco = sessionStorage.getItem("COMODANTEENDERECO");
     // const comandatario = sessionStorage.getItem("COMANDATARIO");
     // const comandatariocpf = sessionStorage.getItem("COMANDATARIOCPF");
     // const comandatarioenderaco = sessionStorage.getItem("COMANDATARIOENDERECO");
@@ -19,5 +19,5 @@ window.addEventListener('load', () =>{
     document.getElementById('result-comodanteendereco').innerHTML = comodanteendereco;
     document.getElementById('result-comandatario').innerHTML = comandatario;
     document.getElementById('result-comandatariocpf').innerHTML = comandatariocpf;
-    document.getElementById('result-comandatarioenderaco').innerHTML = comandatarioenderaco;
-} ) 
+    document.getElementById('result-comandatarioendereco').innerHTML = comandatarioendereco;
+}) 
