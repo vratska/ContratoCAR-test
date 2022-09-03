1879384640f929fde1e791a8288166ac8679e0bf
Elias <mr.vratska@gmail.com>
2022-09-03 11:23:53 -0300

    alteração no metodo de preenchiemento

--- result.js
+++ result.js
@@ -1,18 +1,18 @@
 window.addEventListener('load', () =>{
-    // const params = (new URL(document.location)).searchParams;
-    // const comodante = params.get('comodante');
-    // const comodantecpf = params.get('comodantecpf');
-    // const comodanteenderaco = params.get('comodanteenderaco');
-    // const comendatario = params.get('comendatario');
-    // const comendatariocpf = params.get('comendatariocpf');
-    // const comendatarioenderaco = params.get('comendatarioenderaco');
+    const params = (new URL(document.location)).searchParams;
+    const comodante = params.get('comodante');
+    const comodantecpf = params.get('comodantecpf');
+    const comodanteenderaco = params.get('comodanteenderaco');
+    const comandatario = params.get('comendatario');
+    const comandatariocpf = params.get('comendatariocpf');
+    const comandatarioenderaco = params.get('comendatarioenderaco');
 
-    const comodante = sessionStorage.getItem("COMODANTE");
-    const comodantecpf = sessionStorage.getItem("COMODANTECPF");
-    const comodanteenderaco = sessionStorage.getItem("COMODANTEENDERECO");
-    const comandatario = sessionStorage.getItem("COMANDATARIO");
-    const comandatariocpf = sessionStorage.getItem("COMANDATARIOCPF");
-    const comandatarioenderaco = sessionStorage.getItem("COMANDATARIOENDERECO");
+    // const comodante = sessionStorage.getItem("COMODANTE");
+    // const comodantecpf = sessionStorage.getItem("COMODANTECPF");
+    // const comodanteenderaco = sessionStorage.getItem("COMODANTEENDERECO");
+    // const comandatario = sessionStorage.getItem("COMANDATARIO");
+    // const comandatariocpf = sessionStorage.getItem("COMANDATARIOCPF");
+    // const comandatarioenderaco = sessionStorage.getItem("COMANDATARIOENDERECO");
 
     document.getElementById('result-comodante').innerHTML = comodante;
     document.getElementById('result-comodantecpf').innerHTML = comodantecpf;
